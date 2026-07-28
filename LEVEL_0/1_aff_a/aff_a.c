cd1cc01e76b91ccfb6206e0194dd1d96312633a2
Aziz <azizbahssou444@gmail.com>
2026-07-29 00:17:52 +0100

    firt one

--- LEVEL_0/1_aff_a/aff_a.c
+++ LEVEL_0/1_aff_a/aff_a.c
@@ -2,5 +2,20 @@
 
 int	main(int ac, char **av)
 {
+	int	i;
 
+	if (ac != 1)
+	{
+		write(1, "a\n",2);
+		return(0);
+	}
+	i = 0;
+	while (av[1][i]){
+		if(av[1][i] == 'a'){
+			write(1,&av[1][i],1);
+			break;
+		}
+		i++;
+	}
+	write(1, "\n",1);
 }
