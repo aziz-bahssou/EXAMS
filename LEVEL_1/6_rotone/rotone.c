7c179e6563156891accf84184faa0ad32979af5b
Aziz <azizbahssou444@gmail.com>
2026-08-13 23:47:26 +0100

    done

--- LEVEL_1/6_rotone/rotone.c
+++ LEVEL_1/6_rotone/rotone.c
@@ -1,6 +1,42 @@
 #include <unistd.h>
 
+char enigma(char c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		if(c == 'z')
+			return('a');
+		else
+			return(c + 1);
+	}
+	else
+	{
+		if(c == 'Z')
+			return('A');
+		else
+			return(c + 1);
+	}
+}
+
 int	main(int ac, char **av)
 {
+	int i;
+	if(ac != 2)
+	{
+		write(1, "\n",1);
+		return (0);
+	}
+	i = 0;
+	while(av[1][i])
+	{
+		char c;
+		c = av[1][i];
 
+		if((av[1][i] >= 'A' && av[1][i] <= 'Z')
+		|| av[1][i] >= 'a' && av[1][i] <= 'z')
+			c = enigma(c);
+		write(1, &c,1);
+		i++;
+	}
+	write(1, "\n",1);
 }
--- LEVEL_1/8_fizzbuzz/fizzbuzz.c
+++ LEVEL_1/8_fizzbuzz/fizzbuzz.c
@@ -1,6 +1,32 @@
 #include <unistd.h>
 
+void _put_nmber(int nb)
+{
+	if(nb > 9)
+		_put_nmber(nb / 10);
+	char c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
 int	main(void)
 {
+	int i;
 
+	i = 1;
+	while(i <= 100)
+	{
+		if(i % 3 == 0 && i % 5 == 0)
+			write(1, "fizzbuzz", 8);
+
+		else if(i % 5 == 0)
+			write(1, "buzz",4);
+
+		else if(i % 3 == 0)
+			write(1, "fizz",4);
+			
+		else
+			_put_nmber(i);
+		write(1, "\n",1);
+		i++;
+	}
+	write(1, "\n",1);
 }
