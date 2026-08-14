891a5546e4c939fdf5b459216fd9f38ee194cf9b
Aziz <azizbahssou444@gmail.com>
2026-08-14 23:57:55 +0100

    done

--- LEVEL_1/12_first_last_char/first_last_char.c
+++ LEVEL_1/12_first_last_char/first_last_char.c
@@ -1,6 +1,27 @@
 #include <unistd.h>
-
+void _putchar(char c)
+{
+	write(1, &c, 1);
+}
 int	main(int ac, char **av)
 {
+	if(ac != 2 || av[1][0] == '\0')
+	{
+		write(1, "\n",1);
+		return(0);
+	}
+	int i ;
+	i = 0;
+	while(av[1][i])
+		i++;
+	if(i > 1)
+	{
+		_putchar(av[1][0]);
+		_putchar(av[1][--i]);
+		return (0);
+	}
+	_putchar(av[1][0]);
+	_putchar(av[1][0]);
+	return(0);
 
 }
