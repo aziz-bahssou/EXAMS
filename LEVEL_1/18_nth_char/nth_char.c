31f683db872a09392e4dff78c1282a0d956196b9
Aziz <azizbahssou444@gmail.com>
2026-08-15 23:59:38 +0100

    done

--- LEVEL_1/18_nth_char/nth_char.c
+++ LEVEL_1/18_nth_char/nth_char.c
@@ -2,5 +2,28 @@
 
 void	print_nth_char(char *str, int n)
 {
+	if(str[0] == '\0' || n < 0)
+	{
+		write(1, "\n",1);
+		return(0);
+	}
+
+	int i;
+	i = 0;
+
+	while(str[i])
+		i++;
+
+	if( n > i)
+	{
+		write(1, "\n",1);
+		return(0);
+	}
+	i = 0;
+	while(str[i])
+	{
+		write(1, str[i],1);
+		i += n;
+	}
 
 }
