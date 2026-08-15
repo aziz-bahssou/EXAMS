d8453cdf171e27502055c018cf3fa1c2a31df735
Aziz <azizbahssou444@gmail.com>
2026-08-15 23:42:51 +0100

    done

--- LEVEL_1/17_sum_digits/sum_digits.c
+++ LEVEL_1/17_sum_digits/sum_digits.c
@@ -1,4 +1,15 @@
 int	sum_digits(char *str)
 {
+	int sum;
+	int i;
 
+	i = 0;
+	sum = 0;
+	while(str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			sum += ((str[i]) - '0');
+		i++;
+	}
+	return(sum);
 }
