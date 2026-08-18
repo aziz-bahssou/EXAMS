011220928e0239f6d525755b62d0bd5420a6d934
Aziz <azizbahssou444@gmail.com>
2026-08-18 21:55:52 +0100

    done

--- LEVEL_1/26_ft_print_params/ft_print_params.c
+++ LEVEL_1/26_ft_print_params/ft_print_params.c
@@ -2,5 +2,20 @@
 
 int	main(int ac, char **av)
 {
+	int i;
+	int j;
 
+	i = 1;
+	while(i < c)
+	{
+		j = 0;
+		while(av[i][j])
+		{
+			write(1,&av[i][j],1);
+			j++;
+		}
+		write(1, "\n",1);
+		i++;
+	}
+	return(0);
 }
--- LEVEL_1/28_ft_factorial/ft_factorial.c
+++ LEVEL_1/28_ft_factorial/ft_factorial.c
@@ -1,4 +1,14 @@
 int	ft_factorial(int nb)
 {
+	if(nb < 0)
+		return (0);
+	if(nb == 0)
+		return(1);
 
+	return(nb * ft_factorial(nb - 1));
+}
+#include <stdio.h>
+int main()
+{
+	printf("%d", ft_factorial(5));
 }
