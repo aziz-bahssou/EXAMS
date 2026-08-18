306cf6f64c947684c4638a73b04a79d1b9074507
Aziz <azizbahssou444@gmail.com>
2026-08-18 23:30:58 +0100

    Done

--- LEVEL_1/30_ft_str_reverse/ft_str_reverse.c
+++ LEVEL_1/30_ft_str_reverse/ft_str_reverse.c
@@ -1,4 +1,21 @@
 char	*ft_str_reverse(char *str)
 {
+	int start;
+	int end;
+	char rev;
 
+	end = 0;
+	while(str[end])
+		end++;
+	end--;
+	start = 0;
+	while(start < end)
+	{
+		rev = str[start];
+		str[start] = str[end];
+		str[end] = rev;
+		start++;
+		end--;
+	}
+	return(str);
 }
--- LEVEL_1/31_ft_swap_chars/ft_swap_chars.c
+++ LEVEL_1/31_ft_swap_chars/ft_swap_chars.c
@@ -1,6 +1,43 @@
 #include <unistd.h>
 
+int get_leen(char *str)
+{
+	int leen;
+	leen = 0;
+	while(str[leen])
+		leen++;
+	return(leen);
+}
+int is_odd(int n)
+{
+	if(n % 2 == 0)
+		return(0);
+	return(1);
+}
+
+char *rev_str(char *str)
+{
+	int end;
+	char c;
+	end = get_leen(str);
+	if(is_odd(get_leen(str)))
+		end -= 2;
+	else
+		end--;
+		
+	while(end)
+	{
+		c = str[end];
+		str[end] = str[end - 1];
+		str[end - 1] = c;
+		end -=2;
+	}
+	return(str);
+}
+#include <stdio.h>
+
 int	main(int ac, char **av)
 {
-
+	char str[] = "helle";
+	printf("%s", rev_str(str));
 }
