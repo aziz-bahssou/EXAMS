fd7843cb78853ef06f0c630a46ed3dfd0b9cdc77
Aziz <azizbahssou444@gmail.com>
2026-08-19 23:57:37 +0100

    done

--- LEVEL_2/0_ft_atoi/ft_atoi.c
+++ LEVEL_2/0_ft_atoi/ft_atoi.c
@@ -1,4 +1,30 @@
 int	ft_atoi(const char *str)
 {
+	int res;
+	int sing;
+	int i;
+	sing = 1;
+	i = 0;
+	while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if(str[i] == '-' || str[i] == '+')
+	{
+		if(str[i] == '-')
+			sing = -1;
+		i++;
+	}
+	res = 0;
+	while(str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
 
+	return(res * sing);
 }
+#include <stdio.h>
+
+int main()
+{
+	printf("%d", ft_atoi("132GG"));
+}	
