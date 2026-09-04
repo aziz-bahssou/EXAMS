dd5ef842ba4b5fcba620a5d9858f16027f299545
Aziz <azizbahssou444@gmail.com>
2026-09-04 17:51:17 +0100

    dine

--- LEVEL_2/12_strcspn/strcspn.c
+++ LEVEL_2/12_strcspn/strcspn.c
@@ -2,5 +2,20 @@
 
 size_t	ft_strcspn(const char *s, const char *reject)
 {
+	size_t i;
+	size_t j;
 
+	i = 0;
+	while(s[i])
+	{
+		j = 0;
+		while(reject[j])
+		{
+			if(reject[j] == s[i])
+				return(i);
+			j++;
+		}
+		i++;
+	}
+	return(i);
 }
