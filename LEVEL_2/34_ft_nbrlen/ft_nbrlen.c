0da75657d9e90df50b822f6defab27c4dadd07a2
Aziz <azizbahssou444@gmail.com>
2026-09-12 23:57:35 +0100

    done

--- LEVEL_2/34_ft_nbrlen/ft_nbrlen.c
+++ LEVEL_2/34_ft_nbrlen/ft_nbrlen.c
@@ -1,4 +1,6 @@
 int	ft_nbrlen(int nb)
 {
-
+	if (nb == 0)
+		return(0);
+	return (1 + ft_nbrlen(nb / 10));
 }
--- LEVEL_2/35_ft_multiply_str/ft_multiply_str.c
+++ LEVEL_2/35_ft_multiply_str/ft_multiply_str.c
@@ -1,6 +1,84 @@
 #include <unistd.h>
 
+int	_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	put_number(int nb)
+{
+	char	c;
+
+	if (nb > 9)
+		put_number(nb / 10);
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
+
 int	main(int ac, char **av)
 {
+	int	i;
+	int	j;
+	int	pos;
+	int	len1;
+	int	len2;
+	int	arr[1000];
 
+	if (ac != 3)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	len1 = _len(av[1]);
+	len2 = _len(av[2]);
+
+	i = 0;
+	while (i < len1 + len2)
+	{
+		arr[i] = 0;
+		i++;
+	}
+
+	i = len1 - 1;
+	pos = 0;
+	while (i >= 0)
+	{
+		j = len2 - 1;
+		while (j >= 0)
+		{
+			arr[i + j + 1] +=
+				(av[1][i] - '0') * (av[2][j] - '0');
+			j--;
+		}
+		i--;
+	}
+
+	i = len1 + len2 - 1;
+	while (i > 0)
+	{
+		if (arr[i] >= 10)
+		{
+			arr[i - 1] += arr[i] / 10;
+			arr[i] %= 10;
+		}
+		i--;
+	}
+
+	i = 0;
+	while (i < len1 + len2 - 1 && arr[i] == 0)
+		i++;
+
+	while (i < len1 + len2)
+	{
+		put_number(arr[i]);
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
 }
