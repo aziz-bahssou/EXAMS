3430a2d8efba5356733626cd3d77b64d2167a0e1
Aziz <azizbahssou444@gmail.com>
2026-09-15 11:36:54 +0100

    done

--- LEVEL_2/37_ft_mirror_str/ft_mirror_str.c
+++ LEVEL_2/37_ft_mirror_str/ft_mirror_str.c
@@ -1,6 +1,27 @@
 #include <unistd.h>
 
+int leen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i])
+		i++;
+	return(i);
+}
 int	main(int ac, char **av)
 {
+	int i;
 
+	if(ac != 2)
+		return(0);
+	i = 0;
+	while(av[1][i])
+		write(1,&av[1][i++],1);
+	write(1, "|",1);
+	i = leen(av[1]);
+	while(i)
+		write(1,&av[1][--i],1);
+	write(1, "\n",1);
+	return(0);
 }
