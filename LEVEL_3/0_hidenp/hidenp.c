f2be5db3a9817fc17d48e9d66f260289064884fa
Aziz <azizbahssou444@gmail.com>
2026-09-15 16:41:55 +0100

    done

--- LEVEL_3/0_hidenp/hidenp.c
+++ LEVEL_3/0_hidenp/hidenp.c
@@ -1,6 +1,34 @@
 #include <unistd.h>
 
+
+int _hidenp(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while(s2[i])
+	{
+		if(s1[j] == s2[i])
+			j++;
+		i++;
+	}
+	return(s1[j] == '\0');
+}
+#include <stdio.h>
+
 int	main(int ac, char **av)
 {
+	char ch;
 
+	if(ac != 3)
+	{
+		write(1, "\n",1);
+		return(0);
+	}
+
+	ch = _hidenp(av[1],av[2]) + '0';
+	write(1, &ch,1);
+	write(1, "\n",1);
 }
