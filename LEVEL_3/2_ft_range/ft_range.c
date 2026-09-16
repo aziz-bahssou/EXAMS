0fd988270401280353234d719069949a9694ddbb
Aziz <azizbahssou444@gmail.com>
2026-09-16 14:32:26 +0100

    done

--- LEVEL_3/2_ft_range/ft_range.c
+++ LEVEL_3/2_ft_range/ft_range.c
@@ -2,5 +2,17 @@
 
 int	*ft_range(int start, int end)
 {
+	int *res;
+	int i;
+	int size;
 
+	size = (end - start) + 1;
+	res = malloc(size * sizeof(int));
+	if(!res)
+		return(NULL);
+
+	i = 0;
+	while(i < size)
+		res[i++] = start++;
+	return(res);
 }
